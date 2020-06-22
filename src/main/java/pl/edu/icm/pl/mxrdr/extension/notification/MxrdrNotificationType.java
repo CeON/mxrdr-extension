072d504e4eb832e66ee13400612477288f02daa7
Karol Kulik <karol191pol@gmail.com>
2020-06-22 13:03:49 +0200

    Closes#1123: Mxrdr notification support. (#24)

---
+++ src/main/java/pl/edu/icm/pl/mxrdr/extension/notification/MxrdrNotificationType.java
@@ -0,0 +1,10 @@
+package pl.edu.icm.pl.mxrdr.extension.notification;
+
+/**
+ * Notification types for mxrdr, the base ones are in {@link edu.harvard.iq.dataverse.persistence.user.NotificationType}.
+ */
+public class MxrdrNotificationType {
+
+    public static final String MXRDR_WORKFLOW_SUCCESS = "MXRDR_WORKFLOW_SUCCESS";
+    public static final String MXRDR_WORKFLOW_FAIL = "MXRDR_WORKFLOW_FAIL";
+}
