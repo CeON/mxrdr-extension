577850878384e4f613c8604a5622c9ce7533bf01
tomaszblazejczyk <blazejc@icm.edu.pl>
2020-09-10 11:07:08 +0200

    Closes #48: In .cbf file metadata importer some values are given in meters, not in milimeters

--- src/main/java/pl/edu/icm/pl/mxrdr/extension/importer/cbf/CbfFileParser.java
+++ src/main/java/pl/edu/icm/pl/mxrdr/extension/importer/cbf/CbfFileParser.java
@@ -5,6 +5,9 @@ import edu.harvard.iq.dataverse.importer.metadata.ResultField;
 import pl.edu.icm.pl.mxrdr.extension.importer.MxrdrMetadataField;
 
 import javax.inject.Singleton;
+
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,13 +33,13 @@ public class CbfFileParser {
             addIfPresentWithResultAdd(MxrdrMetadataField.DETECTOR_TYPE.getValue(), extractDetectorType(cbfLine.toLowerCase(), cbfLine), results);
 
             addIfPresentWithResultAdd(MxrdrMetadataField.DATA_COLLECTION_DETECTOR_DISTANCE.getValue(),
-                                      extractField(cbfLine.toLowerCase(), cbfLine, "detector_distance", 2), dataCollectionChildren);
+                    convertToMilimeters(extractField(cbfLine.toLowerCase(), cbfLine, "detector_distance", 2)), dataCollectionChildren);
             addIfPresentWithResultAdd(MxrdrMetadataField.DATA_COLLECTION_OSCILLATION_STEP_SIZE.getValue(),
                     extractField(cbfLine.toLowerCase(), cbfLine, "angle_increment", 2), dataCollectionChildren);
             addIfPresentWithResultAdd(MxrdrMetadataField.DATA_COLLECTION_DETECTOR_OVERLOAD.getValue(),
                     extractField(cbfLine.toLowerCase(), cbfLine, "count_cutoff", 2), dataCollectionChildren);
             addIfPresentWithResultAdd(MxrdrMetadataField.DATA_COLLECTION_DETECTOR_THICKNESS.getValue(),
-                    extractField(cbfLine.toLowerCase(), cbfLine, "thickness", 4), dataCollectionChildren);
+                    convertToMilimeters(extractField(cbfLine.toLowerCase(), cbfLine, "thickness", 4)), dataCollectionChildren);
             addIfPresentWithResultAdd(MxrdrMetadataField.DATA_COLLECTION_STARTING_ANGLE.getValue(),
                     extractField(cbfLine.toLowerCase(), cbfLine, "start_angle", 2), dataCollectionChildren);
             addIfPresentWithResultAdd(MxrdrMetadataField.DATA_COLLECTION_WAVE_LENGTH.getValue(),
@@ -56,6 +59,21 @@ public class CbfFileParser {
     }
 
     // -------------------- PRIVATE --------------------
+
+    /**
+     * Some fields in .cbf files in their headers have respective values in meters, 
+     * while in metadata we expect it to be in milimeters. (#48)
+     * @param parsedValue Value parsed from file in meters
+     * @return Value in milimeters
+     */
+    private String convertToMilimeters(String parsedValue) {
+        if (StringUtils.isNotEmpty(parsedValue)) {
+            return "" + (Float.parseFloat(parsedValue) * 1000);
+        } else {
+            return "";
+        }
+    }
+    
 
     private String extractDetectorType(String normalizedCbfLine, String originalCbfLine) {
         if (normalizedCbfLine.contains("detector:")) {
