49c2793d49da53a386e154fd4cc0aa06f98e1044
Wojciech Trębicki <trebicki@icm.edu.pl>
2021-09-07 12:51:06 +0200

    Closes #68: Import from XDS.INP

---
+++ src/test/java/pl/edu/icm/pl/mxrdr/extension/importer/xdsinp/XdsInpRunnerTest.java
@@ -0,0 +1,50 @@
+package pl.edu.icm.pl.mxrdr.extension.importer.xdsinp;
+
+import edu.harvard.iq.dataverse.importer.metadata.ImporterFieldKey;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class XdsInpRunnerTest {
+
+    private XdsInpRunner runner = new XdsInpRunner(null);
+
+    @Test
+    void prepareFiles() throws IOException {
+        // given
+        File inputFile = Files.createTempFile("temp-test", "").toFile();
+        Map<ImporterFieldKey, Object> importerInput = Collections.singletonMap(XdsInpFormKeys.INPUT_FILE, inputFile);
+
+        // when
+        XdsInpRunner.ProducedPaths producedPaths = runner.prepareFiles(importerInput);
+
+        // then
+        assertThat(producedPaths.getTempDirectory()).exists();
+        assertThat(producedPaths.getInputFile()).exists();
+        assertThat(producedPaths.getInputFile().getParent()).isEqualTo(producedPaths.getTempDirectory());
+
+        Files.deleteIfExists(producedPaths.getInputFile());
+        Files.deleteIfExists(producedPaths.getTempDirectory());
+    }
+
+    @Test
+    void cleanUp() throws IOException {
+        // given
+        File inputFile = Files.createTempFile("temp-test", "").toFile();
+        Map<ImporterFieldKey, Object> importerInput = Collections.singletonMap(XdsInpFormKeys.INPUT_FILE, inputFile);
+
+        // when
+        XdsInpRunner.ProducedPaths producedPaths = runner.prepareFiles(importerInput);
+        runner.cleanUp(producedPaths);
+
+        // then
+        assertThat(producedPaths.getTempDirectory()).doesNotExist();
+        assertThat(producedPaths.getInputFile()).doesNotExist();
+    }
+}
